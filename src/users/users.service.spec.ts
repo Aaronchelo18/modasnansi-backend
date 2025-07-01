a1e99dd8d8117bfb760671bbbcc30a7d461a7a03
Aaronchelo18 <138612031+Aaronchelo18@users.noreply.github.com>
2025-06-30 23:48:37 -0500

    feat: Finalize SonarQube integration with GitHub Actions x3

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -1,15 +1,47 @@
+// src/users/users.service.spec.ts
+
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { UsersService } from './users.service';
+import { User } from './entities/user.entity';
+import { Permission } from '../permissions/entities/permission.entity';
+
+// Un tipo helper para nuestros mocks de repositorio
+// AÑADIMOS "extends object" PARA SOLUCIONAR EL ERROR DE TYPESCRIPT
+export type MockRepository<T extends object = any> = Partial<Record<keyof Repository<T>, jest.Mock>>;
+
+// Una función para crear un mock de repositorio vacío
+const createMockRepository = (): MockRepository => ({
+  findOne: jest.fn(),
+  find: jest.fn(),
+  create: jest.fn(),
+  save: jest.fn(),
+});
 
 describe('UsersService', () => {
   let service: UsersService;
+  let userRepository: MockRepository;
+  let permissionRepository: MockRepository;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [
+        UsersService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: createMockRepository(),
+        },
+        {
+          provide: getRepositoryToken(Permission),
+          useValue: createMockRepository(),
+        },
+      ],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
+    userRepository = module.get<MockRepository>(getRepositoryToken(User));
+    permissionRepository = module.get<MockRepository>(getRepositoryToken(Permission));
   });
 
   it('should be defined', () => {
